4931ef6c9ea05626e06ace0b2709fd1c503978ad
Aziz <aziz_inc@apps.ipb.ac.id>
2025-01-09 12:01:06 +0700

    add sorted results to the query list

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,31 @@
 package data
 
-import "greenlight.skyespirates.net/internal/validator"
+import (
+	"greenlight.skyespirates.net/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafeList []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
